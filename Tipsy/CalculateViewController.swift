28d85cef4ed8f23272cc18a61a718cabdb8248b1
Alex Rumer <alexrumer@gmail.com>
2020-02-11 20:33:42 -0800

    Steo 2 Merge
    Created iBActions

---
+++ Tipsy/CalculateViewController.swift
@@ -0,0 +1,31 @@
+//
+//  ViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculateViewController: UIViewController {
+    @IBOutlet weak var billTextField: UITextField!
+    
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: Any) {
+    }
+    
+    @IBAction func calculatePressed(_ sender: UIButton) {
+    }
+    
+    
+
+}
+
