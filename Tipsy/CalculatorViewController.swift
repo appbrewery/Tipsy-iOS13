83bc7ba5e5bea990413f0bc5ad96842b47153b54
Tam Phan <tam.phan@mac.com>
2020-04-09 23:10:18 -1000

    IBOutlets and IBActions connected

--- Tipsy/CalculatorViewController.swift
+++ Tipsy/CalculatorViewController.swift
@@ -15,14 +15,18 @@ class CalculatorViewController: UIViewController {
 	@IBOutlet weak var tenPctButton: UIButton!
 	@IBOutlet weak var twentyPctButton: UIButton!
 	@IBOutlet weak var splitNumberLabel: UILabel!
-
-	@IBAction func tipChanged(_ sender: Any) {
+	
+	@IBAction func tipChanged(_ sender: UIButton) {
+		zeroPctButton.isSelected = true
+		//tenPctButton.isSelected = false
+		//twentyPctButton.isSelected = false
+	
 	}
 	
 	@IBAction func stepperValueChanged(_ sender: Any) {
 	}
 	
-	@IBAction func calculatorPressed(_ sender: Any) {
+	@IBAction func calculatePressed(_ sender: Any) {
 	}
 }
 
