1dca05f2d04ae5acd52b2c44e05b43507a4ec70d
Tam Phan <tam.phan@mac.com>
2020-04-12 21:27:11 -1000

    First set of features
    grab tip percent, print to console when calculate button pressed.

--- Tipsy/CalculatorViewController.swift
+++ Tipsy/CalculatorViewController.swift
@@ -15,18 +15,37 @@ class CalculatorViewController: UIViewController {
 	@IBOutlet weak var tenPctButton: UIButton!
 	@IBOutlet weak var twentyPctButton: UIButton!
 	@IBOutlet weak var splitNumberLabel: UILabel!
+
+	var tip = 0.10
 	
 	@IBAction func tipChanged(_ sender: UIButton) {
-		zeroPctButton.isSelected = true
-		//tenPctButton.isSelected = false
-		//twentyPctButton.isSelected = false
+		zeroPctButton.isSelected = false
+		tenPctButton.isSelected = false
+		twentyPctButton.isSelected = false
+		
+		// Highlight selected button
+		sender.isSelected = true
+		
+		// Grab button title
+		let buttonTitle = sender.currentTitle!
+		
+		// Remove % sign from the end of button title that was grabbed
+		let buttonTitleMinusPercentSign = String(buttonTitle.dropLast())
+		
+		// Change String into Double
+		let buttonTitleAsANumber = Double(buttonTitleMinusPercentSign)!
+		
+		// Turn tip into decimal
+		tip = buttonTitleAsANumber / 100
 	
 	}
 	
 	@IBAction func stepperValueChanged(_ sender: Any) {
+		
 	}
 	
 	@IBAction func calculatePressed(_ sender: Any) {
+		print(tip)
 	}
 }
 
