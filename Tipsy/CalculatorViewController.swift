7aca1fb3dbeac746083f60d245da8759330e1be8
Tam Phan <tam.phan@mac.com>
2020-04-17 00:15:06 -1000

    Lesson 148 completed

--- Tipsy/CalculatorViewController.swift
+++ Tipsy/CalculatorViewController.swift
@@ -17,35 +17,64 @@ class CalculatorViewController: UIViewController {
 	@IBOutlet weak var splitNumberLabel: UILabel!
 
 	var tip = 0.10
+	var numberOfPeople: Int = 0
+	var billTotal = 0.0
 	
 	@IBAction func tipChanged(_ sender: UIButton) {
 		zeroPctButton.isSelected = false
 		tenPctButton.isSelected = false
 		twentyPctButton.isSelected = false
 		
-		// Highlight selected button
+		// Highlight selected button.
 		sender.isSelected = true
 		
-		// Grab button title
+		// Grab button title.
 		let buttonTitle = sender.currentTitle!
 		
-		// Remove % sign from the end of button title that was grabbed
+		// Remove % sign from the end of button title that was grabbed.
 		let buttonTitleMinusPercentSign = String(buttonTitle.dropLast())
 		
-		// Change String into Double
+		// Change String into Double.
 		let buttonTitleAsANumber = Double(buttonTitleMinusPercentSign)!
 		
-		// Turn tip into decimal
+		// Turn tip into decimal.
 		tip = buttonTitleAsANumber / 100
 	
 	}
 	
-	@IBAction func stepperValueChanged(_ sender: Any) {
+	@IBAction func stepperValueChanged(_ sender: UIStepper) {
+		
+		// Update var splitValue to label.
+		numberOfPeople = Int(sender.value)
+		
+		// Update bill split number label.
+		splitNumberLabel.text = String(numberOfPeople)
 		
 	}
 	
-	@IBAction func calculatePressed(_ sender: Any) {
+	@IBAction func calculatePressed(_ sender: UIButton) {
 		print(tip)
+		print(numberOfPeople)
+		
+		// Get text from billTextField. This did not work.
+		// billTotal = Double(billTextField.text?)
+		
+		// Storing user inputted string from billTextField into a new constant.
+		let bill = billTextField.text!
+		
+		// If the text is not an empty string.
+		if bill != "" {
+			
+			// Convert string to double.
+			billTotal = Double(bill)!
+			
+			// Calculate amount including tip, split between number of people.
+			let result = billTotal * (1 + tip) / Double(numberOfPeople)
+			
+			// Convert double to string and format to 2 decimal places
+			let resultTo2DecimalPlace = String(format: "%.2f", result)
+			
+		}
 	}
 }
 
