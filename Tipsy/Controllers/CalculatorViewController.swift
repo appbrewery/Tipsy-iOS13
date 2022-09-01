fa04d4c918eed76aea5ad1e603cbab6694e5b2e0
bhishaksanyal <sanyalbishak93@gmail.com>
2022-09-01 21:34:38 +0530

    Tip selection

---
+++ Tipsy/Controllers/CalculatorViewController.swift
@@ -0,0 +1,41 @@
+//
+//  CalculatorViewController.swift
+//  Tipsy
+//
+//  Created by Angela Yu on 09/09/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class CalculatorViewController: UIViewController {
+    
+    @IBOutlet weak var billTextField: UITextField!
+    @IBOutlet weak var zeroPctButton: UIButton!
+    @IBOutlet weak var tenPctButton: UIButton!
+    @IBOutlet weak var twentyPctButton: UIButton!
+    @IBOutlet weak var splitNumberLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func tipChanged(_ sender: UIButton) {
+        zeroPctButton.isSelected = false
+        tenPctButton.isSelected = false
+        twentyPctButton.isSelected = false
+        sender.isSelected = true
+        print("TAG \(sender.tag)")
+    }
+    
+    @IBAction func stepperValueChanged(_ sender: Any) {
+        
+    }
+    
+    @IBAction func calculateButtonPressed(_ sender: Any) {
+        
+    }
+    
+}
+
---
+++ Tipsy/Controllers/ResultsViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ResultsViewController.swift
+//  Tipsy
+//
+//  Created by Bhishak Sanyal on 01/09/22.
+//  Copyright © 2022 The App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ResultsViewController: UIViewController {
+
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var settingsLabel: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func recalculatePressed(_ sender: Any) {
+        
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
